bef9c3bed7a6c6372d9380f620e068e6ade337c9
Dmitriy Korotkov <dimka-13-01@mail.ru>
2024-01-19 01:57:29 +0300

    Завершает сетку страницы index.html

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -112,3 +112,33 @@ navLink.onclick = function() {
   window.addEventListener('load', initialize);
   window.addEventListener('resize', initialize);
 })();
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  // карта
+  const ymaps = window.ymaps;
+  ymaps.ready(() => {
+    // Указывается идентификатор HTML-элемента.
+    const myMap = new ymaps.Map('map', {
+        center: [59.938679, 30.3230044],
+        zoom: 14,
+        controls: []
+      }),
+
+      myPlacemark = new ymaps.Placemark(myMap.getCenter(), {}, {
+        // Опции.
+        // Необходимо указать данный тип макета.
+        iconLayout: 'default#image',
+        // Своё изображение иконки метки.
+        iconImageHref: 'images/map-pin.png',
+        // Размеры метки.
+        iconImageSize: [57, 53],
+        // Смещение левого верхнего угла иконки относительно
+        // её "ножки" (точки привязки).
+        iconImageOffset: [-26, -42]
+      });
+
+    myMap.geoObjects.add(myPlacemark);
+  });
+
+});
